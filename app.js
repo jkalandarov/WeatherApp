1af1b68e6975a66eb1243b52ce23cbaec4384c94
jkalandarov <jkalandarov@gmail.com>
2021-08-22 14:44:31 +0500

    Added function for city search, need to show data by city search on front-end

--- app.js
+++ app.js
@@ -19,77 +19,15 @@
 //     //$('#barcelona-weather').text("Weather condition: " + condition);
 //     $('#barcelona-weather-icon').attr('src', icon);
 //   })
-
-//   //Milan
-//   var city = "Milan"
-//   const milan_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=metric';
-  
-//   //Retrieving data from API URL
-//   $.get(milan_url, (data, status)=>{
-//     const weatherData = data;
-//     const temp = weatherData.main.temp;
-//     const feel = weatherData.main.feels_like;
-//     const condition = weatherData.weather[0].description;
-//     const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
-    
-//     $('#milan_temp').text("Current temperature: " + temp + "°C");
-//     $('#milan_feel').text("Feels like: " + feel + "°C");
-//     //$('#barcelona-weather').text("Weather condition: " + condition);
-//     $('#milan-weather-icon').attr('src', icon);
-//   })
-
-//   //Brussels
-//   var city = "Brussels"
-//   const brussels_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=metric';
-  
-//   //Retrieving data from API URL
-//   $.get(brussels_url, (data, status)=>{
-//     const weatherData = data;
-//     const temp = weatherData.main.temp;
-//     const feel = weatherData.main.feels_like;
-//     const condition = weatherData.weather[0].description;
-//     const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
-    
-//     $('#brussels_temp').text("Current temperature: " + temp + "°C");
-//     $('#brussels_feel').text("Feels like: " + feel + "°C");
-//     //$('#barcelona-weather').text("Weather condition: " + condition);
-//     $('#brussels-weather-icon').attr('src', icon);
-//   })
-
-//   //Frankfurt
-//   var city = "Frankfurt"
-//   const frankfurt_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=metric';
-  
-//   //Retrieving data from API URL
-//   $.get(frankfurt_url, (data, status)=>{
-//     const weatherData = data;
-//     const temp = weatherData.main.temp;
-//     const feel = weatherData.main.feels_like;
-//     const condition = weatherData.weather[0].description;
-//     const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
-    
-//     $('#frankfurt_temp').text("Current temperature: " + temp + "°C");
-//     $('#frankfurt_feel').text("Feels like: " + feel + "°C");
-//     //$('#barcelona-weather').text("Weather condition: " + condition);
-//     $('#frankfurt-weather-icon').attr('src', icon);
-//   })
-
-//   //London
-//   var city = "London"
-//   const london_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + appKey + '&units=metric';
-  
-//   //Retrieving data from API URL
-//   $.get(london_url, (data, status)=>{
-//     const weatherData = data;
-//     const temp = weatherData.main.temp;
-//     const feel = weatherData.main.feels_like;
-//     const condition = weatherData.weather[0].description;
-//     const icon = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
-    
-//     $('#london_temp').text("Current temperature: " + temp + "°C");
-//     $('#london_feel').text("Feels like: " + feel + "°C");
-//     //$('#barcelona-weather').text("Weather condition: " + condition);
-//     $('#london-weather-icon').attr('src', icon);
-//   })
   
 // });
+
+let search = document.querySelector('input')
+let mainCard = document.getElementById('main')
+let searchCard = document.getElementById('searchCard')
+
+search.addEventListener('submit', ()=>{
+    mainCard.style.display = 'none'
+    searchCard.style.display = 'inline-block'
+    console.log("Event triggered")
+})
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const getPhotoUrl =(city) => {
     return photo_url
 }
 
-const getWeather = async (lon, lat) => {
+const getWeatherByCoordinates = async (lon, lat) => {
     let weather_key = "f2a12517c2c9fb2178d509a1d040dba5"
     let weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_key}&units=metric`
 
@@ -29,7 +29,32 @@ const getWeather = async (lon, lat) => {
         }
     })
 
-    console.log('axios-------------------------------------------')
+    let weather = {
+            city: weather_json.name,
+            country: weather_json.sys.country,
+            temperature: Math.round(weather_json.main.temp),
+            humidity: weather_json.main.humidity,
+            icon: weather_json.weather[0].icon,
+            photo: photo,
+            description: weather_json.weather[0].description
+        }
+
+    return weather;
+}
+
+const getWeatherByCityName = async (city) => {
+    let weather_key = "f2a12517c2c9fb2178d509a1d040dba5"
+    let weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weather_key}&units=metric`
+
+    const {data: weather_json} = await axios(weather_url)
+    const photo = await axios(getPhotoUrl(weather_json.name)).then(res => {
+        if(res.data.total != 0) {
+            return res.data.results[0].urls.small
+        } else {
+            return 'https://www.plantemoran.com/-/media/images/insights-images/2018/04/thinking-about-becoming-a-smart-city.jpg?h=704&w=1100&hash=D5677DCC5CE6DF0C080CFAB8CC3EB10E'
+
+        }
+    })
 
     let weather = {
             city: weather_json.name,
@@ -45,16 +70,25 @@ const getWeather = async (lon, lat) => {
 }
 
 app.get("/", (req, res) => {
+    
     res.render("weather");
+})
+
+app.post('/', async (req, res)=>{
+    
+    let searchData = await getWeatherByCityName(req.body.searchBar)
+    console.log(searchData)
+    return res.json(searchData);
+    
 })
 
 app.post("/default", async (req, res) => {
     var longitude = req.body.longitude;
     var latitude = req.body.latitude;
 
-    let data = await getWeather(longitude, latitude)
+    let data = await getWeatherByCoordinates(longitude, latitude)
 
-    console.log(data)
+    //console.log(data)
     return res.json(data);
 })
 
